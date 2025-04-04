171e30ed11e99d6176fc1102d5bfdac8c2ea47d9
Utkarsh Naman <truly.away@gmail.com>
2025-04-04 20:30:11 +0530

    changed color theme

--- React-Landing-Page/src/components/services.jsx
+++ React-Landing-Page/src/components/services.jsx
@@ -7,8 +7,7 @@ export const Services = (props) => {
         <div className="section-title">
           <h2>Our Services</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            We provide a range of services from the collection of scrap to re selling it to the vendors. We also provide a platform for the vendors to sell their scrap and earn money.
           </p>
         </div>
         <div className="row">
