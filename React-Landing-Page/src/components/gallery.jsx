bb3b6eeaed12d88ec84388dcc2f631841ebcc150
YashGupta404 <yashgupta.work.2005@gmail.com>
2025-04-04 21:17:52 +0530

    Changed gallery text

--- React-Landing-Page/src/components/gallery.jsx
+++ React-Landing-Page/src/components/gallery.jsx
@@ -8,8 +8,7 @@ export const Gallery = (props) => {
         <div className="section-title">
           <h2>Gallery</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            A glimpse of our work and the projects we have been involved in.
           </p>
         </div>
         <div className="row">
--- React-Landing-Page/src/components/navigation.jsx
+++ React-Landing-Page/src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            Menu
+            ScrapDeal
           </a>{" "}
         </div>
 
